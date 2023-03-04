8c90dd34e408bdfdc1eb6c7dc3bd2c3bc584a0d6
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-03-04 00:27:57 -0300

    fastify cookies management

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,18 +2,26 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { db } from './../database';
+import { checkSessionId } from '../middleware/check-session-id';
 
 
 export async function trasactionsRoutes(app: FastifyInstance) {
 
-    app.get('/', async () => {
-        const transactions = await db('transactions').select('*');
+    app.get('/', { preHandler: [checkSessionId] }, async (request) => {
+        const { sessionId } = request.cookies;
+
+        const transactions = await db('transactions')
+            .where('session_id', sessionId)
+            .select();
+
         return {
             transactions
         };
     });
 
-    app.get('/:id', async (request) => {
+    app.get('/:id', { preHandler: [checkSessionId] }, async (request) => {
+        const { sessionId } = request.cookies;
+
         const getTransactionsParamsSchema = z.object({
             id: z.string().uuid(),
         });
@@ -24,19 +32,30 @@ export async function trasactionsRoutes(app: FastifyInstance) {
 
         const { id } = params;
 
-        const transaction = await db('transactions').where('id', id).first();
+
+        const transaction = await db('transactions')
+            .where({
+                session_id: sessionId,
+                id: id
+            })
+            .first();
 
         return {
             transaction
         };
     });
 
-    app.get('/summary', async () => {
-        const summary = await db('transactions').sum('amount', { as: 'amount' }).first();
+    app.get('/summary', { preHandler: [checkSessionId] }, async (request) => {
+        const { sessionId } = request.cookies;
+
+        const summary = await db('transactions')
+            .where('session_id', sessionId)
+            .sum('amount', { as: 'amount' })
+            .first();
         return { summary };
     });
 
-    app.post('/', async (request, response) => {
+    app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
             amount: z.number(),
@@ -51,11 +70,11 @@ export async function trasactionsRoutes(app: FastifyInstance) {
 
         // Cookie sessionId
         let sessionId = request.cookies.sessionId;
-        
-        if(!sessionId) {
+
+        if (!sessionId) {
             sessionId = randomUUID();
-            response.cookie('sessionId', sessionId, {
-                path:'/',
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
                 maxAge: 1000 * 60 * 60 * 24, // 7 days
             });
         }
@@ -63,9 +82,10 @@ export async function trasactionsRoutes(app: FastifyInstance) {
         await db('transactions').insert({
             id: randomUUID(),
             title,
-            amount: type === 'credit' ? amount : amount * -1
+            amount: type === 'credit' ? amount : amount * -1,
+            session_id: sessionId
         });
 
-        return response.status(201).send();
+        return reply.status(201).send();
     });
 }
